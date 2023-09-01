4a1e8d6817a8a29899013ef1ea655ad2d3d47e66
SaidNivek <kevin.vi.dias@gmail.com>
2023-08-31 23:09:28 -0400

    Stub out the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import styles from './style'
+
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
-      <div className="">
-
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
       </div>
+
+
+      
     </div>
 )
 
