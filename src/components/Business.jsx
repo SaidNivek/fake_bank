b03f386a94fda8b56b767d9f44a80d6d39d39e64
SaidNivek <kevin.vi.dias@gmail.com>
2023-09-10 20:16:13 -0400

    Work on the business component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,10 +1,14 @@
 import { features } from "../constants"
-import styles, {layout} from '../style'
+import styles, { layout } from '../style'
 import Button from './Button'
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id="features" className={`${layout.section}`} >
+      <div className={`${layout.sectionInfo}`} >
+        <h2 className={`${styles.heading2}`}>You do the business, <br className="sm:block hidden" />we'll handle the money.</h2>
+      </div>
+    </section>
   )
 }
 
