5a8f7e2eb56f2766860b9956eb4d8bdd117a653b
SaidNivek <kevin.vi.dias@gmail.com>
2023-09-10 20:25:33 -0400

    Begin creating a FeatureCard component within Business, only using this comp here

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,6 +2,14 @@ import { features } from "../constants"
 import styles, { layout } from '../style'
 import Button from './Button'
 
+const FeatureCard = ({icon, title, content, index}) => (
+  <div>
+    <div>
+      <img src={icon} alt="icon" />
+    </div>
+  </div>
+)
+
 const Business = () => {
   return (
     <section id="features" className={`${layout.section}`} >
@@ -10,6 +18,13 @@ const Business = () => {
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
         <Button styles="mt-10" />
       </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
+
     </section>
   )
 }
