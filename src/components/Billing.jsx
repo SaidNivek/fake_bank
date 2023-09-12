e18654c3d55105e26931327bc326d62d61cc4dda
SaidNivek <kevin.vi.dias@gmail.com>
2023-09-12 16:02:31 -0400

    Begin Billing component

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,12 @@
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+import { apple, bill, google } from '../assets'
+import styles, { layout } from '../style' 
+
+const Billing = () => (
+  <section id="product" className={`${layout.sectionReverse}`}>
+    <div className={`${layout.sectionImgReverse}`}>
+
+    </div>
+  </section>
+)
 
 export default Billing
