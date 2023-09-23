e76abc62459cb96f958f852079eb2c27e04e6881
SaidNivek <kevin.vi.dias@gmail.com>
2023-09-22 21:38:00 -0400

    Remove index from testimonials to remove warning

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -14,7 +14,7 @@ const Testimonials = () => (
     </div>
 
     <div className='flex flex-wrap sm:justify-start jusitfy-center w-full feedback-container relative z-[1]' >
-      {feedback.map((card, index) => (
+      {feedback.map((card) => (
         <FeedbackCard key={card.id}  {...card}/>
       ))}
     </div>
