61fdf5a0d9bd81892ce4b758390e9835a66b69eb
SaidNivek <kevin.vi.dias@gmail.com>
2023-09-27 11:31:19 -0400

    Add some spacing to the links in Footer comp

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,10 +17,10 @@ const Footer = () => (
         <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-p mt-10 ">
           {footerLinks.map((footerLink) => (
             <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px] ">
-              <h4 className="font-poppins font-medium text-[18px] leading[27px] text-white ">{footerLink.title}</h4>
-              <ul>
+              <h4 className="font-poppins font-medium text-[18px] leading[27px] text-white">{footerLink.title}</h4>
+              <ul className="list-none mt-4">
                 {footerLink.links.map((link, index) => (
-                  <li key={link.name} className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ">
+                  <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length - 1 ? 'mb-4' : 'mb-0'}`}>
                     {link.name}
                   </li>
                 ))}
